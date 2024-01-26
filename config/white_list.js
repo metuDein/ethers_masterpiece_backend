4277a8d6e484e9b098790ed7debe337d7132b78a
metu dein <maxmetudein.com>
2024-01-26 23:26:24 +0100

    update

--- config/white_list.js
+++ config/white_list.js
@@ -1,6 +1,8 @@
 const White_List = [
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://ethersmasterpiece.com',
+    'https://ethersmasterpiece.com',
 ]
 
 module.exports = White_List
