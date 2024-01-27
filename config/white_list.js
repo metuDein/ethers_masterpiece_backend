087d07f109da8177c85e4c52a61f5eeb80e8fec7
metu dein <maxmetudein.com>
2024-01-27 18:26:05 +0100

    update

--- config/white_list.js
+++ config/white_list.js
@@ -3,6 +3,7 @@ const White_List = [
     'http://localhost:3000',
     'http://ethersmasterpiece.com',
     'https://ethersmasterpiece.com',
+    'https://ethersmasterpiece.netlify.app'
 ]
 
 module.exports = White_List
