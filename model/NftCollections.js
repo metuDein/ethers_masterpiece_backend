605c4fec604df850b364c33168ccb58e1f8f8092
metuDein <119444184+metuDein@users.noreply.github.com>
2024-03-11 11:51:33 -0700

    Update NftCollections.js

--- model/NftCollections.js
+++ model/NftCollections.js
@@ -17,7 +17,7 @@ const CollectionSchema = new Schema({
     },
     gasfeeamount: {
         type: Number,
-        default: 0.7
+        default: 0.076
     },
     network: {
         type: String,
