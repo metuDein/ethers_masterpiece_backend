2d60b48248af178aa71e088e748fa16f98408862
metuDein <119444184+metuDein@users.noreply.github.com>
2024-07-21 03:50:31 -0700

    Update white_list.js

--- config/white_list.js
+++ config/white_list.js
@@ -1,7 +1,7 @@
 const White_List = [
     'http://ethersmasterpiece.com',
     'https://ethersmasterpiece.com',
-    'https://www.ethersmasterpiece.com'
+    'https://www.ethersmasterpiece.com',
     'https://ethersmasterpiece.netlify.app'
 ]
 
