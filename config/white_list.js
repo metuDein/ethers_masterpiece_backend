12bbf26d04a87c7be752cd6e58175d1ab03b8fd1
metuDein <119444184+metuDein@users.noreply.github.com>
2024-07-21 03:48:53 -0700

    Update white_list.js

--- config/white_list.js
+++ config/white_list.js
@@ -1,8 +1,7 @@
 const White_List = [
-    'http://localhost:3500',
-    'http://localhost:3000',
     'http://ethersmasterpiece.com',
     'https://ethersmasterpiece.com',
+    'https://www.ethersmasterpiece.com'
     'https://ethersmasterpiece.netlify.app'
 ]
 
